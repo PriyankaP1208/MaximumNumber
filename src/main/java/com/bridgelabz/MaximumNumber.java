a6271ad4847dded8814d9ef7d18fc0354bbcbee5
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-09 09:42:48 +0530

    ADDED:Print the maximum value

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -3,10 +3,15 @@ package com.bridgelabz;
 public class MaximumNumber <T extends Comparable<T>> {
     T[] values;
 
+    public static <T> void printMaximum(T maximum){
+        System.out.println("Maximum:" + maximum);
+    }
+
     @SafeVarargs
     public MaximumNumber(T... values){
         this.values = values;
     }
+
     public static <T extends Comparable<T>> T max(T... values) {
         T maximum = values[0];
         for (int i = 1; i < values.length; i++)
@@ -19,6 +24,8 @@ public class MaximumNumber <T extends Comparable<T>> {
     }
 
     public T maximumValue() {
-        return max(values);
+       T maximum = max(values);
+       printMaximum(maximum);
+       return maximum;
     }
 }
